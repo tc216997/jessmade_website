a5aa9751aa7e75ab7fbd0da5e811221847cbfebe
Terry Chong <tc216997@gmail.com>
2017-06-08 07:38:00 +0800

    added bottom up contact form

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,15 @@
-$('.menu').on('click', function(){
-  if ($('.l-site').hasClass('is-open')) {
-    $('.menu').removeClass('is-active');
-  	$('.l-site').removeClass('is-open');
-  } else {
-    $('.menu').addClass('is-active');
-  	$('.l-site').addClass('is-open');
-  }
+$(document).ready(function() {
+  $( "#contact-btn" ).click(function() {
+      $( ".contact-div" ).slideToggle( "slow");
+      if($('.contact-div').hasClass('hide')){
+          $('#contact-btn').animate({bottom: '0px'}, 'slow');
+          $('.contact-div').removeClass('hide').addClass('show');
+      } else {
+          $('#contact-btn').animate({bottom: '62vh'}, 'slow');
+          $('.contact-div').removeClass('show').addClass('hide');
+          $('.contact-div').css('display', 'block');
+      }
+  });
+  //$('#contact-btn').click();
 });
 
