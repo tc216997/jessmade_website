be2033c7b9df3b2e03696ad9f5f58403c0888c99
Terry Chong <tc216997@gmail.com>
2017-06-08 22:29:14 +0800

    contact form changes

--- public/js/index.js
+++ public/js/index.js
@@ -4,10 +4,19 @@ $(document).ready(function() {
       if($('.contact-div').hasClass('hide')){
           $('#contact-btn').animate({bottom: '0px'}, 'slow');
           $('.contact-div').removeClass('hide').addClass('show');
+          $('a').css('cursor', 'pointer');
+          $('a').off('click').on('click', function(e){
+            return true;
+          });
       } else {
           $('#contact-btn').animate({bottom: '70vh'}, 'slow');
           $('.contact-div').removeClass('show').addClass('hide');
           $('.contact-div').css('display', 'block');
+          $('a').css('cursor', 'default');
+          $('a').click(function(e) {
+            e.preventDefault();
+            return false;
+          });
       }
   });
   //$('#contact-btn').click();
