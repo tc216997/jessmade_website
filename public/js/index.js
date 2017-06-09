05c35be0d3c11c2c0d007b185db09ce438c22f86
Terry Chong <tc216997@gmail.com>
2017-06-09 22:59:00 +0800

    setup emailing system

--- public/js/index.js
+++ public/js/index.js
@@ -1,34 +1,123 @@
 $(document).ready(function() {
   $( "#contact-btn" ).click(function() {
       $( ".contact-div" ).slideToggle( "slow");
-      if($('.contact-div').hasClass('hide')){
-          $('#contact-btn').animate({bottom: '0px'}, 'slow');
-          $('.contact-div').removeClass('hide').addClass('show');
-          $('a').css('cursor', 'pointer');
-          $('a').off('click').on('click', function(e){
-            return true;
-          });
-      } else {
-          $('#contact-btn').animate({bottom: '72vh'}, 'slow');
-          $('.contact-div').removeClass('show').addClass('hide');
-          $('.contact-div').css('display', 'block');
-          $('a').css('cursor', 'default');
-          $('a').click(function(e) {
-            e.preventDefault();
-            return false;
-          });
-      }
+      contactToggle();
   });
-  $('#contact-btn').click();
+  //$('#contact-btn').click();
 });
 
 $(document).ready(function() {
-  $('#submit-button').click(function(e){
+  $('form').on('submit', function(e){
     //show modal, prevent redirect
     e.preventDefault();
     $('.modal').css('display', 'block');
-    // prep ajax object and send to server
-    console.log('prep ajax object to send to server');
+    // show loading icon
+    showSendingMsg();
+    // check if request is allowed
+
+    // if allowed change the form data into json object
+    let formData = serializeJson($(this).serializeArray());
+    $.ajax({
+      url:'/send-email',
+      type:'POST',
+      data: formData,
+      datatype: 'json',
+      error: function(response) {
+        console.log(response.status);
+        console.log(response.statusText);
+        console.log(response.responseJSON.status);
+        console.log(response.responseJSON.error);
+        errorHandler();
+      },
+      success: function(response) {
+        console.log(response);
+        console.log(response.status);
+        successHandler();
+      }
+    });
   });
 });
 
+function contactToggle() {
+    if($('.contact-div').hasClass('hide')){
+      $('#contact-btn').animate({bottom: '0px'}, 'slow');
+      $('.contact-div').removeClass('hide').addClass('show');
+      $('a').css('cursor', 'pointer');
+      $('a').off('click').on('click', function(e){
+        return true;
+      });
+    } else {
+      $('#contact-btn').animate({bottom: '72vh'}, 'slow');
+      $('.contact-div').removeClass('show').addClass('hide');
+      $('.contact-div').css('display', 'block');
+      $('a').css('cursor', 'default');
+      $('a').click(function(e) {
+        e.preventDefault();
+        return false;
+      });
+    }  
+}
+
+function showSendingMsg() {
+  $('#sending-icon').css('display', 'block');
+  $('#sending-message').css('display', 'block');  
+}
+
+function hideSendingMsg() {
+  $('#sending-icon').css('display', 'none');
+  $('#sending-message').css('display', 'none');  
+}
+
+function successHandler() {
+  hideSendingMsg();
+  $('#success-icon').css('display', 'block');
+  $('#success-message').css('display', 'block');
+  window.setTimeout(() => {
+    $('.modal').css('display', 'none');
+    $('#success-icon').css('display', 'none');
+    $('#success-message').css('display', 'none');
+    $( ".contact-div" ).slideToggle( "slow");
+    contactToggle();
+    clearForm();
+  }, 3000);  
+}
+
+function errorHandler() {
+  hideSendingMsg();
+  $('#error-icon').css('display', 'block');
+  $('#error-message').css('display', 'block');
+  window.setTimeout(() => {
+    $('.modal').css('display', 'none');
+    $('#error-icon').css('display', 'none');
+    $('#error-message').css('display', 'none');
+  }, 3000);
+}
+
+function hideErrorMsg() {
+  $('#error-icon').css('display', 'none');
+  $('#error-message').css('display', 'none');    
+}
+
+function serializeJson(array) {
+  let json = {};
+  array.map(item => {
+    json[item.name] = item.value;
+  });
+  return json;
+}
+
+function formTest(){
+  $('#form-name').val('Terry Chong');
+  $('#form-number').val('123-456-7799');
+  $('#form-email').val('tchong916@gmail.com');
+  $('#form-subject').val('Test email');
+  $('#form-message').val('What is the price of 1 box of macarons?');
+}
+
+function clearForm(){
+  $('#form-name').val('');
+  $('#form-number').val('');
+  $('#form-email').val('');
+  $('#form-subject').val('');
+  $('#form-message').val('');  
+}
--- server.js
+++ server.js
@@ -11,10 +11,45 @@ app.use(compression());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static(path.resolve(__dirname, 'public')));
 
+let transport = nodemailer.createTransport({
+  service: 'Gmail',
+  auth: {
+    user:process.env.EMAILUSER,
+    pass:process.env.EMAILPW
+  }
+});
+
 app.get(/^\/(index)?$/, (req, res) => {
   res.sendFile(getFile('index'));
+});
+
+app.post('/send-email', (req, res) => {
+  let clientName = req.body.name;
+  let clientNumber = req.body.number;
+  let clientEmail= req.body.email;
+  let clientSubject = req.body.subject;
+  let clientMessage = req.body.message;
+  sendMail(clientName, clientNumber, clientEmail, clientSubject, clientMessage, res);
 });
 
 app.listen(app.get('port'), () => {
   console.log('server listening at port ' + app.get('port'));
 });
+
+function sendMail(name, number, email, subject, message, res) {
+  let mailSettings = {
+    to: process.env.FORWARDADDRESS,
+    subject: subject,
+    html: '<strong>Customer name: </strong>  ' + name + '<br><br><strong>Customer email address:  </strong>' + email  + '<br><br><strong>Phone number: </strong>' + number + 
+    '<br><br><strong>Message:  </strong><br>' + message,
+  }
+  transport.sendMail(mailSettings, (err, info) => {
+    if (err) {
+      console.log(err);
+      res.status(500).json({status:'Email wasn\'t sent!', error:err});
+    } else {
+      console.log('email %s sent: %s', info.messageId, info.response);
+      res.status(200).json({status:'Email was sent successfully!', error:null}); 
+    }
+  });
+}
